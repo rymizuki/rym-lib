30d63f844a3d3ada052207ab460a3801c3db59cd
mizuki_r <ry.mizuki@gmail.com>
2025-05-10 10:53:03 +0900

    refactor(query-module-driver-sequelize): use query-module-sql-builder

--- packages/query-module-driver-sequelize/src/driver.ts
+++ packages/query-module-driver-sequelize/src/driver.ts
@@ -1,11 +1,15 @@
-import { createBuilder, createConditions, SQLBuilderPort } from 'coral-sql'
 import { QueryTypes, Sequelize } from 'sequelize'
 
 import {
-  QueryDriverInterface,
   QueryCriteriaInterface,
+  QueryDriverInterface,
   QueryLoggerInterface,
 } from '@rym-lib/query-module'
+import {
+  buildSQL,
+  createBuilder,
+  SQLBuilderPort,
+} from '@rym-lib/query-module-sql-builder'
 
 export class QueryDriverSequelize implements QueryDriverInterface {
   private setup: ((builder: SQLBuilderPort) => SQLBuilderPort) | null = null
@@ -28,9 +32,7 @@ export class QueryDriverSequelize implements QueryDriverInterface {
     if (!this.setup) {
       throw new Error('QueryDriver must be required source.')
     }
-    const builder = this.buildCondition(this.setup(createBuilder()), criteria)
-
-    const [sql, replacements] = builder.toSQL()
+    const [sql, replacements] = buildSQL(this.setup(createBuilder()), criteria)
     this.context.logger.verbose(`[QueryDriverSequelize] ${sql}`, {
       sql,
       replacements,
@@ -42,115 +44,4 @@ export class QueryDriverSequelize implements QueryDriverInterface {
     })
     return rows as Record<string, any>[]
   }
-
-  private buildCondition(
-    builder: SQLBuilderPort,
-    criteria: QueryCriteriaInterface,
-  ) {
-    // filter -> where
-    if (criteria.filter) {
-      const whole = createConditions()
-
-      for (const filter of Array.isArray(criteria.filter)
-        ? criteria.filter
-        : [criteria.filter]) {
-        const cond = createConditions()
-
-        for (const name of keys(filter)) {
-          if (typeof name !== 'string') continue
-
-          const property = filter[name]
-          if (!property) continue
-
-          for (const operator of keys(property)) {
-            const value = property[operator]
-            switch (operator) {
-              case 'eq': {
-                // name = value
-                cond.and(name, value)
-                break
-              }
-              case 'ne': {
-                // name != value
-                cond.and(name, '!=', value)
-                break
-              }
-              case 'lt': {
-                // name < value
-                cond.and(name, '<', value)
-                break
-              }
-              case 'lte': {
-                // name <= value
-                cond.and(name, '<=', value)
-                break
-              }
-              case 'gt': {
-                // name > value
-                cond.and(name, '>', value)
-                break
-              }
-              case 'gte': {
-                // name >= value
-                cond.and(name, '>=', value)
-                break
-              }
-              case 'contains': {
-                if (value) {
-                  // name LIKE `%${value}%`
-                  cond.and(name, 'like', `%${value}%`)
-                }
-                break
-              }
-              case 'not_contains': {
-                if (value) {
-                  // name NOT LIKE `%${value}%`
-                  cond.and(name, 'not like', `%${value}%`)
-                }
-                break
-              }
-              case 'in': {
-                // name IN value
-                cond.and(name, 'in', value)
-                break
-              }
-            }
-          }
-        }
-
-        whole.or(cond)
-      }
-      builder.where(whole)
-    }
-
-    // orderBy
-    if (criteria.orderBy) {
-      const { orderBy } = criteria
-      const orders = Array.isArray(orderBy) ? orderBy : [orderBy]
-      for (const order of orders) {
-        const [name, direction] = (order as string).split(':')
-        if (!name) continue
-        builder.orderBy(
-          name,
-          (direction as 'asc' | 'desc' | undefined) ?? 'asc',
-        )
-      }
-    }
-
-    // take -> limit
-    if (criteria.take) {
-      builder.limit(criteria.take)
-    }
-
-    // skip -> offset
-    if (criteria.skip) {
-      builder.offset(criteria.skip)
-    }
-
-    return builder
-  }
-}
-
-function keys<T extends Record<string, unknown>>(value: T) {
-  return Object.keys(value) as (keyof T)[]
 }
